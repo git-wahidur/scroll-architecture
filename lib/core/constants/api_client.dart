bfe776ffce28b108bec161a3ea71b6ea46ffad0e
git-wahid <137379343+git-wahid@users.noreply.github.com>
2026-02-27 16:00:47 +0600

    feat(core): implement api client with dio and add api constants class add endpoints

--- lib/core/constants/api_client.dart
+++ lib/core/constants/api_client.dart
@@ -1,1 +1,68 @@
+import 'package:dio/dio.dart';
 
+import '../constants/api_constants.dart';
+
+class ApiClient {
+  late final Dio _dio;
+  String? _token;
+
+  ApiClient() {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: ApiConstants.baseUrl,
+        connectTimeout: const Duration(seconds: 30),
+        receiveTimeout: const Duration(seconds: 30),
+        headers: {'Content-Type': 'application/json'},
+      ),
+    );
+
+    _dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          if (_token != null) {
+            options.headers['Authorization'] = 'Bearer $_token';
+          }
+          return handler.next(options);
+        },
+        onError: (error, handler) {
+          return handler.next(error);
+        },
+      ),
+    );
+  }
+
+  void setToken(String token) {
+    _token = token;
+  }
+
+  Future<Response> get(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+  }) async {
+    try {
+      return await _dio.get(path, queryParameters: queryParameters);
+    } on DioException catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  Future<Response> post(String path, {dynamic data}) async {
+    try {
+      return await _dio.post(path, data: data);
+    } on DioException catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  Exception _handleError(DioException error) {
+    switch (error.type) {
+      case DioExceptionType.connectionTimeout:
+      case DioExceptionType.receiveTimeout:
+        return Exception('Connection timeout');
+      case DioExceptionType.badResponse:
+        return Exception(error.response?.data['message'] ?? 'Server error');
+      default:
+        return Exception('Network error');
+    }
+  }
+}
--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,1 +1,3 @@
-
+class ApiConstants {
+  static const String baseUrl = 'https://fakestoreapi.com';
+}

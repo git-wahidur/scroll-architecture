948f577d635afc1dabf77c50150ae104c0b92ca0
git-wahid <137379343+git-wahid@users.noreply.github.com>
2026-02-27 16:22:16 +0600

    feat: add app colors setup

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //Primary Colors
+  static const Color primary = Color(0xFFF85606);
+  static const Color primaryDark = Color(0xFFD94A05);
+  static const Color secondary = Color(0xFF0095FF);
+
+  // Background Colors
+  static const Color background = Color(0xFFF5F5F5);
+  static const Color cardBackground = Colors.white;
+
+  // Text Colors
+  static const Color textPrimary = Color(0xFF212121);
+  static const Color textSecondary = Color(0xFF757575);
+  static const Color subtextOnPrimary = Color(0xFFD94A05);
+  static const Color textHint = Color(0xFFBDBDBD);
+
+  // Status Colors
+  static const Color success = Color(0xFF4CAF50);
+  static const Color error = Color(0xFFF44336);
+  static const Color warning = Color(0xFFFF9800);
+
+  // Divider
+  static const Color divider = Color(0xFFE0E0E0);
+
+  // Rating
+  static const Color star = Color(0xFFFFB400);
+}
